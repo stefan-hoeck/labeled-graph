fc3bbb3feb799f294101dc3569f8bfe7433c6f21
stefan-hoeck <hock@zhaw.ch>
2015-03-24 04:52:42 +0100

    more edges tests

--- test/EdgeTests.hs
+++ test/EdgeTests.hs
@@ -9,12 +9,14 @@ import Data.Graph.Simple.Vertex
 import GraphQC
 import Test.Framework
 
-prop_edgeMay ∷ Vertex → Vertex → Bool
-prop_edgeMay v1 v2 = isNothing (edgeMay v1 v2) == (v1 == v2)
+import qualified Data.Set as S
 
 {- First we check that all Edge constructors give the same
    internal representation. Afterwards we only need to
    test a single constructor -}
+prop_edgeMay ∷ Vertex → Vertex → Bool
+prop_edgeMay v1 v2 = isNothing (edgeMay v1 v2) == (v1 == v2)
+
 prop_edgeMay_likeUnsafeEdge ∷ Vertex → Vertex → Property
 prop_edgeMay_likeUnsafeEdge v1 v2 = v1 /= v2 ==> edgeMay v1 v2 == Just (unsafeEdge v1' v2')
     where v1' = min v1 v2
@@ -73,3 +75,44 @@ prop_edgeAdjacent ∷ Edge → Edge → Bool
 prop_edgeAdjacent e1 e2 = let vs1 = edgeVertices e1
                               vs2 = edgeVertices e2
                           in  edgesAdjacent e1 e2 == any (`elem` vs1) vs2
+
+
+{- Edges -}
+
+prop_edges_valid ∷ Edges → Bool
+prop_edges_valid = validEdges
+
+prop_edgesFromList_valid ∷ [Edge] → Bool
+prop_edgesFromList_valid = validEdges . edgesFromList
+
+prop_completeEdges_valid ∷ Small Int → Bool
+prop_completeEdges_valid = validEdges . completeEdges . abs . getSmall
+
+prop_completeEdges_size ∷ Small Int → Bool
+prop_completeEdges_size (Small n) = let n' = abs n
+                                        len = edgesSize $ completeEdges n'
+                                    in  len == (n' * (n'-1)) `div` 2
+
+prop_completeEdges_max ∷ Small Int → Property
+prop_completeEdges_max (Small n) = let n' = abs n
+                                       es = unEdges $ completeEdges n'
+                                       mx = maximum es
+                                   in  n' > 1 ==> mx == ((n'-2) <-> (n'-1))
+
+prop_chainEdges_valid ∷ Small Int → Bool
+prop_chainEdges_valid = validEdges . chainEdges . abs . getSmall
+
+prop_chainEdges_size ∷ Small Int → Property
+prop_chainEdges_size (Small n) = let n' = abs n
+                                     len = edgesSize $ chainEdges n'
+                                 in  n' > 1 ==> len == n' - 1
+
+prop_chainEdges_max ∷ Small Int → Property
+prop_chainEdges_max (Small n) = let n' = abs n
+                                    es = unEdges $ chainEdges n'
+                                    mx = maximum es
+                                in  n' > 1 ==> mx == ((n'-2) <-> (n'-1))
+
+validEdges ∷ Edges → Bool
+validEdges es = let es' = unEdges es
+                in  es' == S.toList (S.fromList es')
