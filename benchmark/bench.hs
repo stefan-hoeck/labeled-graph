031bf5bfdb01426b5a10064a48e3750e59fc4ae1
stefan-hoeck <hock@zhaw.ch>
2015-06-12 15:28:30 +0200

    finished DFS benchmarking

--- benchmark/bench.hs
+++ benchmark/bench.hs
@@ -8,18 +8,19 @@ import Data.Graph.Simple
 
 main ∷ IO ()
 main = defaultMain [
-         bgroup "dfs_complete" $ fmap b_dfs_c    [100,1000]
-       , bgroup "dff_complete" $ fmap b_dff_c    [100,1000]
-       , bgroup "dfs_empty"    $ fmap b_dfs_e    [100,1000]
-       , bgroup "dff_empty"    $ fmap b_dff_e    [100,1000]
-       , bgroup "complete"     $ fmap b_complete [100,1000]
+         bgroup "dfs_complete"       $ fmap b_dfs_c    [10,100,1000]
+       , bgroup "dff_complete"       $ fmap b_dff_c    [10,100,1000]
+       , bgroup "dfs_empty"          $ fmap b_dfs_e    [10,100,1000]
+       , bgroup "dff_empty"          $ fmap b_dff_e    [10,100,1000]
        ]
 
 b_dfs_c ∷ Int → Benchmark
-b_dfs_c n = bench (show n) $ nf (dfs $ complete n) [0]
+b_dfs_c n = let c = complete n
+            in  bench (show n) $ nf (dfs c) (vertices c)
 
 b_dff_c ∷ Int → Benchmark
-b_dff_c n = bench (show n) $ nf (dff $ complete n) [0]
+b_dff_c n = let c = complete n
+            in  bench (show n) $ nf (dff c) (vertices c)
 
 b_dfs_e ∷ Int → Benchmark
 b_dfs_e n = let e = empty n
@@ -28,6 +29,3 @@ b_dfs_e n = let e = empty n
 b_dff_e ∷ Int → Benchmark
 b_dff_e n = let e = empty n
             in bench (show n) $ nf (dff e) (vertices e)
-
-b_complete ∷ Int → Benchmark
-b_complete n = bench (show n) $ nf complete n
